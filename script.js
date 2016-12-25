a4843420d4fe037594c7c5dd129ba85a62ffc3e9
Michał Gacek <gacek.michael@outlook.com>
2016-12-25 02:06:27 +0100

    Some fixes to script.js

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
+var name = prompt("What is your name ?"),
+	contentElement = document.getElementById("content"),
+	contentElement2 = document.getElementById("content2"),
+	age;
 
-	var name = prompt("What is your name ?");
-	alert("Welcome " + name + " !!!");
-	var age = prompt ("How old are you " + name + " ?");
+alert("Welcome " + name + " !!!");
+age = prompt ("How old are you " + name + " ?");
 
-	if (age < 18) {
-		alert("I am sorry " + name + " you can't enter our website.")
-		console.log(name + " is " + age + " years old so content is BLOCKED")
-		document.getElementById("content").innerHTML = "CONTENT BLOCKED";
-		document.getElementById("content").style.color = "red"
-		document.getElementById("content2").style.visibility = "hidden";
-	} else {
-		alert("Welcome on our website " + name + " !!!")
-		console.log(name + " is " + age + " years old so content is AVAILABLE")
-		document.getElementById("content").innerHTML = "CONTENT AVAILABLE";
-		document.getElementById("content").style.color = "green"
-		document.getElementById("content2").style.visibility = "visible";
-	}
+if (age < 18) {
+	alert("I am sorry " + name + " you can't enter our website.")
+	console.log(name + " is " + age + " years old so content is BLOCKED")
+	contentElement.innerHTML = "CONTENT BLOCKED";
+	contentElement.style.color = "red"
+	contentElement2.style.visibility = "hidden";
+} else {
+	alert("Welcome on our website " + name + " !!!")
+	console.log(name + " is " + age + " years old so content is AVAILABLE")
+	contentElement.innerHTML = "CONTENT AVAILABLE";
+	contentElement.style.color = "green"
+	contentElement2.style.visibility = "visible";
+}
